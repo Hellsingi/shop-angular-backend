05ab9ae3a50b7dea025b0fe8cb6ff03dc27815bc
Hellsingi <il.dzemchanka@gmail.com>
2022-10-06 22:35:33 +0200

    fix: problem with lambda

--- import-service/src/functions/import-file-parser/index.ts
+++ import-service/src/functions/import-file-parser/index.ts
@@ -5,8 +5,8 @@ export default {
   events: [
     {
       s3: {
-        method: process.env.BUCKET_NAME,
-        path: 's3:ObjectCreated',
+        bucket: process.env.BUCKET_NAME,
+        event: 's3:ObjectCreated:*',
         rules: [
           {
             prefix: 'uploads/',
