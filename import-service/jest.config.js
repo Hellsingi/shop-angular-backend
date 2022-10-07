839a97f8f832d8a95faa5417e38acbb06e06d6c5
Hellsingi <il.dzemchanka@gmail.com>
2022-10-07 14:01:07 +0200

    feat: add test

--- import-service/jest.config.js
+++ import-service/jest.config.js
@@ -1,5 +1,6 @@
 /** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
-const { pathsToModuleNameMapper } = require('ts-jest/utils');
+const { pathsToModuleNameMapper } = require('ts-jest')
+
 const { compilerOptions } = require('./tsconfig.paths.json')
 module.exports = {
   preset: 'ts-jest',
